985891e98dae36e9ed12f9ba209fb1ed67010b0d
keerthanavenkatesan <vekeerthana@gmail.com>
2016-06-25 00:18:54 +0530

    Update evennumbers.C

--- evennumbers.C
+++ evennumbers.C
@@ -1,1 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int a,b;
+printf("enter two numbers");
+scanf("%d %d",&a,&b);
+if(a==b)
+printf("no numbers between")
+else
+{
+for(int i=a+1;i<b;i++)
+{
+if(i%2==0)
+printf("%d\t",i);
+}
+}
+}
 
