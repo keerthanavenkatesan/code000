8734aba4d6724314a6fc8a91669974a5093be0fe
keerthanavenkatesan <vekeerthana@gmail.com>
2016-06-25 00:13:26 +0530

    Update oddnumbers.C

--- oddnumbers.C
+++ oddnumbers.C
@@ -4,9 +4,14 @@ void main()
 int a,b;
 printf("enter two numbers");
 scanf("%d %d",&a,&b);
+if(a==b)
+printf("no numbers between")
+else
+{
 for(int i=a+1;i<b;i++)
 {
 if(i%2!=0)
 printf("%d\t",i);
+}
 }
 }
